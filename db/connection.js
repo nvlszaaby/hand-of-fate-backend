dd126076752097726b1c4d5b32ed7a637880ac41
Gebby Novalisza <gebbynovalisza@student.telkomuniversity.ac.id>
2024-12-25 15:26:12 +0700

    change db.js for deployment mode

--- db/connection.js
+++ db/connection.js
@@ -2,14 +2,14 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   // local
-  user: "postgres",
-  host: "localhost",
-  database: "hof_db2",
-  password: "datalovers",
-  port: 5432,
+  // user: "postgres",
+  // host: "localhost",
+  // database: "hof_db2",
+  // password: "datalovers",
+  // port: 5432,
 
   // deployed
-  //   connectionString: process.env.DB_URL,
+  connectionString: process.env.DB_URL,
 });
 
 module.exports = pool;
